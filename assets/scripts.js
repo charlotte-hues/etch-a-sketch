d99675d65c46fbf059ef90be4ddbb657981bc508
Charlotte Hughes <hues.charlotte@gmail.com>
2019-12-07 18:45:01 +0000

    Added a button and function that resets the grid

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,18 @@
 const grid = document.querySelector('#sketchGrid');
+let gridSize = 256;
+
+function reset() { 
+    // gridSize = prompt('What size Grid?');
+
+    const divs = document.querySelectorAll('.gridDiv');
+    divs.forEach( function(el) {
+        el.classList.remove('color1');
+    });
+
+ }
 
 function createGrid() {
-    for (let i=0, n=1; i<256; i++, n++) { 
+    for (let i=0, n=1; i<gridSize; i++, n++) { 
         let i = document.createElement('div');
         i.classList.add('gridDiv');
         i.id = n;
@@ -17,9 +28,10 @@ const gridDivs = document.querySelectorAll('.gridDiv');
 function addHover(e) {
     console.log(e.target)
     let div = e.target;
-
-    if (div)
-    div.classList.add('hover')
+    div.classList.add('color1')
 } 
 
-console.log(gridDivs);
+const clearButton = document.getElementById('clearButton').addEventListener('click', reset);
+
+
+
