d000afd349f766d9b7c0b408f0c6b59fa6f42de2
Charlotte Hughes <hues.charlotte@gmail.com>
2019-12-07 20:07:46 +0000

    Added button with function to change the size the number of cells in the grid

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,37 +1,50 @@
 const grid = document.querySelector('#sketchGrid');
-let gridSize = 256;
-
-function reset() { 
-    // gridSize = prompt('What size Grid?');
-
-    const divs = document.querySelectorAll('.gridDiv');
-    divs.forEach( function(el) {
-        el.classList.remove('color1');
-    });
-
- }
+let size = 10;
+const gridDivs = document.querySelectorAll('.gridDiv');
 
 function createGrid() {
-    for (let i=0, n=1; i<gridSize; i++, n++) { 
-        let i = document.createElement('div');
-        i.classList.add('gridDiv');
-        i.id = n;
-        i.addEventListener('mouseenter', addHover);
-        grid.appendChild(i);
-    };    
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i=0; i<(size*size); i++) { 
+        let div = document.createElement('div');
+        div.classList.add('gridDiv');
+        div.id = (i+1);
+        div.addEventListener('mouseenter', addHover);
+        grid.appendChild(div);
+    };  
 }
 
 createGrid();
 
-const gridDivs = document.querySelectorAll('.gridDiv');
+function removeGrid() {    
+    for (let i=0; i<(size*size); i++) {
+        const myNode = document.querySelector(".gridDiv");
+        grid.removeChild(myNode); 
+    }
+}
 
 function addHover(e) {
-    console.log(e.target)
+    console.log(e.target.id)
     let div = e.target;
     div.classList.add('color1')
 } 
 
+// BUTTONS
+
 const clearButton = document.getElementById('clearButton').addEventListener('click', reset);
+
+function reset() { 
+    const divs = document.querySelectorAll('.gridDiv');
+    divs.forEach( function(el) { el.classList.remove('color1'); });
+ }
+
+ const changeSizeButton = document.getElementById('changeGridSize').addEventListener('click', resizeGrid);
+
+ function resizeGrid() {  
+    removeGrid();
+    size = prompt('WHAT SIZE?');
+    createGrid();
+ }
 
 
 
